54a315e2a3017303a9b49fecccf09de44ba095a0
kwren96 <katherine.wren196@gmail.com>
2019-06-04 11:21:30 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", () => { 
+  click()
 });
+
+document.getElementById("create-task-form").elements[1].addEventListener("click", function(event){
+  event.preventDefault()
+  createNewTask()
+})
+
+
+
+function createNewTask() {
+  const tasks = document.getElementById('tasks')
+  let newElement = document.createElement('li')
+  tasks.appendChild(newElement)
+  let newTask = document.getElementById('new-task-description').value
+  newElement.innerHTML = newTask
+}
+
