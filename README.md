f6171f757bce52ad804b8fbbb7b052c3a13c554b
kwren96 <katherine.wren196@gmail.com>
2019-06-06 10:48:29 +0100

    Done.

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 
 ## Deliverables:
-
-- A delete function that will remove tasks from your list
-- A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
   - As a challenge, implement a sorting functionality that displays the tasks ASC/DESC based on priority
 - An additional input field (e.g. user, duration, date due)
 - Ability to edit tasks
-- Something of your choice! The main objective is to add a feature that allows the user's input to affect the DOM
-
-[example]: https://learn-co-curriculum.github.io/js-task-lister-lite/
--- src/index.js
+++ src/index.js
@@ -1,19 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => { 
-  click()
 });
 
-document.getElementById("create-task-form").elements[1].addEventListener("click", function(event){
+let priority = "high"
+
+let prioritySelect = "desc"
+
+document.getElementById("create-task-form").elements[1].addEventListener("change", function(event){
+  priority = event.target.value
+})
+
+document.getElementById("create-task-form").elements[2].addEventListener("click", function(event){
   event.preventDefault()
   createNewTask()
 })
 
+const list = document.getElementById("list")
 
+list.querySelector('select').addEventListener("change", function(event){
+  prioritySelect = event.target.value
+  sortByPriority()
+})
 
 function createNewTask() {
+  let newTask = document.getElementById('new-task-description').value
+  if (newTask == "") {}
+  else {
   const tasks = document.getElementById('tasks')
   let newElement = document.createElement('li')
-  tasks.appendChild(newElement)
-  let newTask = document.getElementById('new-task-description').value
+  let deleteButton = document.createElement("span")
+  deleteButton.setAttribute('id', newTask)
+  deleteButton.innerHTML = "X"
+  deleteButton.addEventListener('click', function del(){
+    tasks.removeChild(newElement)
+    tasks.removeChild(deleteButton)
+  })
+  tasks.appendChild(newElement) 
+  tasks.appendChild(deleteButton)
   newElement.innerHTML = newTask
+  if (priority == "high"){
+    newElement.style.color = "red"
+  }
+  else if (priority == "medium") {
+    newElement.style.color = "yellow"
+  }
+  else {
+    newElement.style.color = "green"
+  }
+
+}
 }
 
+function sortByPriority() {
+  
+}
